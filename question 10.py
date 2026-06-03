173324ed03849e8a57237cee69aa30af4e45a9b3
Hariom Patidar <hariompatidar977053@gmail.com>
2026-06-03 15:46:08 +0530

    question 1o

---
+++ question 10.py
@@ -0,0 +1,20 @@
+# Find Minimum Number in List
+num = [12,22,11,33,21,55,76]
+min = num[0]
+for i in num:
+    if i < min:
+        min = i
+print(min)
+
+
+
+
+
+
+
+num1 = [4994,84,33,2,133,2234,657]
+min1 = num1[0]
+for i in num1:
+    if i < min1:
+        min1 = i
+print(min1)
