25a1ff41886153eb85aa53cc75ce4fc2b8391be0
Hariom Patidar <hariompatidar977053@gmail.com>
2026-08-21 20:19:31 +0530

    question 28

---
+++ question 28.py
@@ -0,0 +1,14 @@
+# Find missing number in sequence (1 to n)
+# a = [1,2,4,5,7]
+# n = len(a) + 1
+
+# for i in range(1, n+1):
+#     if i not in a:
+#         print(i)
+        
+a = [1,2,6,9]
+n = len(a) + 1
+
+for i in range(1,a[3]):
+    if i not in a:
+     print(i)
