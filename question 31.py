e2e4f4b32d5f917358b485dd5fd19676a9647f5d
Hariom Patidar <hariompatidar977053@gmail.com>
2026-08-24 07:41:49 +0530

    question 31

---
+++ question 31.py
@@ -0,0 +1,14 @@
+#  Double Each Element in List
+
+a = [2,3,4,5,6,7,8,9]
+for i in range (len(a)):
+    a[i] = a[i] * 2
+print(a)
+    
+    
+    
+c = [2,3,4,5,6,7,8,9]
+d = []
+for i in c:
+    d.append(i * 2)
+print(d)
