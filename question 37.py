ea1f9ac4d6fbc2a67aa6072914b474f9618b6911
Hariom Patidar <hariompatidar977053@gmail.com>
2026-09-17 12:26:08 +0530

    question 37

---
+++ question 37.py
@@ -0,0 +1,14 @@
+# Count Even & Odd
+
+lst = [1, 2, 3, 4, 5, 6]
+
+even = 0
+odd = 0
+
+for num in lst:
+    if num % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+
+print("Even:", even, "Odd:", odd)
